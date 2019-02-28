64555d5b967c4ec545fde8e60fe7159301c7a5d2
Markus Napp <mnapp@suse.com>
2019-02-28 13:35:00 +0100

    Add draft capability to PDF template

--- makepdf.sh
+++ makepdf.sh
@@ -3,7 +3,7 @@
 asciidoctor-pdf \
 -a toc-title=Contents \
 -a chapter-label= \
--a pdf-style=suse \
+-a pdf-style=$2 \
 -a pdf-stylesdir=$SUSEDOC/pdf_template \
 -a icons=font \
 -a pdf-fontsdir=$SUSEDOC/pdf_template/fonts \
